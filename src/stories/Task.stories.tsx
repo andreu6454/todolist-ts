26a3708ee2ca65a15f66abb7b621b7feb1e29fc5
Andrey Malin <andreu6454@gmail.com>
2022-11-26 19:08:01 +0300

    Task story with ReduxStoreProviderDecorator

---
+++ src/stories/Task.stories.tsx
@@ -0,0 +1,25 @@
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import React from "react";
+import TaskItem from "../Components/TaskItem";
+import {ReduxStoreProviderDecorator} from "./Decorators/ReduxStoreProviderDecorator";
+
+export default {
+    title: "TODOLIST/TaskItem",
+    component: TaskItem,
+    decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof TaskItem>
+
+const Template: ComponentStory<typeof TaskItem> = (args ) => <TaskItem {...args}/>;
+
+export const TaskIsDone = Template.bind({})
+
+TaskIsDone.args = {
+    task: {id: "aaa", isDone: true, title: "ASD"},
+    todolistId: "string"
+}
+export const TaskIsNotDone = Template.bind({})
+
+TaskIsNotDone.args = {
+    task: {id: "aaa", isDone: false, title: "ASD"},
+    todolistId: "string"
+}
