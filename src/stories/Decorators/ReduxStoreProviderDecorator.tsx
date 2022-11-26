9506f7b7d0459f7386c1ba672e81da7481f2bb25
Andrey Malin <andreu6454@gmail.com>
2022-11-26 19:25:57 +0300

    Initial state added

--- src/stories/Decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/Decorators/ReduxStoreProviderDecorator.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 import {Provider} from "react-redux";
-import {store} from "../../state/store";
+import {AppRootStateType} from "../../state/store";
+import {combineReducers, legacy_createStore} from "redux";
+import {tasksReducer} from "../../state/tasks-reducer";
+import {todolistsReducer} from "../../state/todolists-reducer";
 
-export const ReduxStoreProviderDecorator = (storyFn: ()=> JSX.Element) => {
-    return <Provider store={store}> {storyFn()}</Provider>
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+
+const initialGlobalState = {
+    todolists: [
+        {id: "todolistId1", title: "What to buy?", filter: "all"},
+        {id: "todolistId2", title: "What to learn?", filter: "all"}
+    ],
+    tasks:{
+        "todolistId1":[
+            {id: '1', title: 'Milk', isDone: true},
+            {id: '2', title: 'Car', isDone: false},
+            {id: '3', title: 'Water', isDone: false}],
+        "todolistId2":[
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}]
+    }
+}
+
+export const storybookStore = legacy_createStore(rootReducer, initialGlobalState as AppRootStateType)
+export const ReduxStoreProviderDecorator = (storyFn: () => JSX.Element) => {
+    return <Provider store={storybookStore}> {storyFn()}</Provider>
 }
