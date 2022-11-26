695e31315c06fa8a5f1839853d86050ebd5d75ad
Andrey Malin <andreu6454@gmail.com>
2022-11-26 19:07:50 +0300

    App story with ReduxStoreProviderDecorator

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,14 @@
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import React from "react";
+import App from "../App";
+import {ReduxStoreProviderDecorator} from "./Decorators/ReduxStoreProviderDecorator";
+
+export default {
+    title: "TODOLIST/App",
+    component: App,
+    decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof App>
+
+const Template: ComponentStory<typeof App> = () =><App/>;
+
+export const AppStory = Template.bind({})
