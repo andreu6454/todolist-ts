f25ca96d88f37e5430795e7a33bbfa1a24982f91
Andrey Malin <andreu6454@gmail.com>
2022-11-26 17:29:19 +0300

    AddItemForm story added

---
+++ src/stories/AddItemForm.stories.tsx
@@ -0,0 +1,23 @@
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import React from "react";
+import {AddItemForm} from "../Components/AddItemForm";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: "TODOLIST/AddItemForm",
+    component: AddItemForm,
+    argTypes: {
+        callBack: {
+            description: 'Clicked'
+        }
+    }
+
+} as ComponentMeta<typeof AddItemForm>
+
+const Template: ComponentStory<typeof AddItemForm> = (args ) => <AddItemForm {...args}/>;
+
+export const AddItemFormStory = Template.bind({})
+
+AddItemFormStory.args = {
+    callBack: action('Clicked Button')
+}
