8d4c898839fe7ba23c3e2aa7107e92e531adcad5
Andrey Malin <andreu6454@gmail.com>
2022-11-29 22:06:10 +0300

    small changes

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./state/store";
+import {v1} from "uuid";
 
-ReactDOM.render(<Provider store={store}> <App /> </Provider>,document.getElementById('root'));
+ReactDOM.render(<Provider store={store} key={v1()}> <App /> </Provider>,document.getElementById('root'));
 
 serviceWorker.unregister();
