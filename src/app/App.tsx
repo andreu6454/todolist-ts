ae5359769e145ebe6d4be2cde5c5c74808041a7d
Andrey Malin <andreu6454@gmail.com>
2022-12-05 19:33:03 +0300

    routes and login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,8 @@ import {TaskType} from "../api/todolist-api";
 import {RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../Components/ErrorSnackBar/ErrorSnackBar";
 import TodolistsList from "../features/TodolistsList/TodolistsList";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Login} from "../features/Login/Login";
 
 
 export type TasksStateType = {
@@ -23,7 +25,12 @@ function App() {
             <ErrorSnackbar/>
             {status === "loading" && <LinearProgress/>}
             <Container fixed>
-                <TodolistsList/>
+                <Routes>
+                    <Route path={'/'} element={ <TodolistsList/>}/>
+                    <Route path={'/login'} element={ <Login/>}/>
+                    <Route path={'/404'} element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path={'*'} element={<Navigate to={'/404'}/>}/>
+                </Routes>
             </Container>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,14 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./state/store";
 import {v1} from "uuid";
+import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<Provider store={store} key={v1()}> <App /> </Provider>,document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store} key={v1()}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 
 serviceWorker.unregister();
