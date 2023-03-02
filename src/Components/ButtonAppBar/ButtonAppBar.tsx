d7359602f52f9113c947c1cfb7d5142f0706ce2a
Andrey Malin <andreu6454@gmail.com>
2023-03-02 23:42:22 +0300

    - добавлена кнопка редиректа на главную - исправлены ошибки

--- src/Components/ButtonAppBar/ButtonAppBar.tsx
+++ src/Components/ButtonAppBar/ButtonAppBar.tsx
@@ -1,21 +1,24 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import React from 'react';
 import {useAppDispatch, useAppSelector} from "../../state/store";
 import {logoutTC} from "../../features/Login/auth-reducer";
+import {Navigate, NavLink} from "react-router-dom";
 
 const ButtonAppBar = () => {
-    const isLoggined = useAppSelector<boolean>(state => state.auth.isLoggedIn)
+    const isLogined = useAppSelector<boolean>(state => state.auth.isLoggedIn)
     const dispatch = useAppDispatch()
     const logOutHandler = () => {
         dispatch(logoutTC())
     }
 
+    const navigateHandle = () => {
+        return <Navigate to={'/'}/>
+    }
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" style={{background: "#3b3939"}}>
@@ -26,13 +29,12 @@ const ButtonAppBar = () => {
                         color="inherit"
                         aria-label="menu"
                         sx={{ mr: 2 }}
+                        onClick={navigateHandle}
                     >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        TodoLists
-                    </Typography>
-                    {isLoggined && <Button color="inherit" onClick={logOutHandler}>Log out</Button>}
+                        <MenuIcon onClick={navigateHandle}/>
+                    </IconButton >
+                    <NavLink className={'todolistLink'} to={'/'}> TodoLists </NavLink>
+                    {isLogined && <Button sx={{ml: 'auto'}} color="inherit" onClick={logOutHandler}>Log out</Button>}
                 </Toolbar>
             </AppBar>
         </Box>
